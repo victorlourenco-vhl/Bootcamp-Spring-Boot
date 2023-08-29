d953b00c740b82d609479cdfc1e7c532af643c22
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-08-29 20:46:22 -0300

    FEAT: Criando a classe CategoryResource

--- Sistema_ERP/src/main/java/com/sistema/entities/Category.java
+++ Sistema_ERP/src/main/java/com/sistema/entities/Category.java
@@ -1,6 +1,11 @@
 package com.sistema.entities;
 
-public class Category {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 
 	private Long id;
 	private String name;
@@ -30,5 +35,24 @@ public class Category {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 
 }
