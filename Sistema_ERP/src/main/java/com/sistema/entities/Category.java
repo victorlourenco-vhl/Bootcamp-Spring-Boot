d4c9674d70d32299b0594dee52fe8791e511f0c5
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-08-29 21:28:45 -0300

    FEAT: Fazendo o mapeamento da entidade Category no JPA com a anotação @Entity

--- Sistema_ERP/src/main/java/com/sistema/entities/Category.java
+++ Sistema_ERP/src/main/java/com/sistema/entities/Category.java
@@ -3,10 +3,18 @@ package com.sistema.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 
