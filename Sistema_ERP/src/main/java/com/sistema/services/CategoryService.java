6b3eab87332e737b8f24c1ba252ed308111175c1
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-08-31 21:41:44 -0300

    FEAT: Retornando o DTO nas consultas ao invés da entidade Category

--- Sistema_ERP/src/main/java/com/sistema/services/CategoryService.java
+++ Sistema_ERP/src/main/java/com/sistema/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.sistema.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sistema.dto.CategoryDTO;
 import com.sistema.entities.Category;
 import com.sistema.repositories.CategoryRepository;
 
@@ -15,8 +17,11 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		return listDto; 
 	}
 
 }
